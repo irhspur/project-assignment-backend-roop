4120eb5d67ed0225a75a541d8a0318c1e0eb3db6
irhspur <bajracharya.roop@gmail.com>
2020-09-11 18:21:23 +0545

    Route: Routes added for clubs, teams and overall matches

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,45 @@
 import express from 'express';
+import api from './services/api';
+import { getTeams } from './utils/dataUtils';
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+app.get('/clubs', async (req, res) => {
+    try {
+        const year = '2015-16'
+        const response = await api.get(`/${year}/en.1.clubs.json`)
+        return res.json(response.data);
+    }
+    catch (err) {
+        return res.json({})
+    }
+});
+
+app.get('/match-details', async (req, res) => {
+    try {
+        const year = '2015-16'
+        const response = await api.get(`/${year}/en.1.json`)
+
+        const teams = getTeams(response.data.rounds)
+        
+        return res.json(response.data);
+    }
+    catch (err) {
+        return res.json({})
+    }
+});
+
+app.get('/teams', async (req, res) => {
+    try {
+        const year = '2015-16'
+        const response = await api.get(`/${year}/en.1.json`)
+        console.log(response.data);
+
+        return res.json(getTeams(response.data.rounds));
+    }
+    catch (err) {
+        return res.json({})
+    }
 });
 
 app.listen(port, () => {
